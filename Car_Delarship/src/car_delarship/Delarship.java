2174c458bdb77a9199ea06c8ec49d0de4b68fab3
Ayush Kumar <ayushshrivastava6041@gmail.com>
2024-08-26 01:05:20 +0530

    Initial commit

---
+++ Car_Delarship/src/car_delarship/Delarship.java
@@ -0,0 +1,30 @@
+package car_delarship;
+
+public class Delarship {
+
+	public static void main(String[] args) {
+		
+		Customer cus1= new Customer();
+		
+		cus1.name="Ayush";
+		cus1.address="Bangalore";
+		cus1.cashOnHand=12000;
+		
+		
+		cus1.purchaseCar(vechile, emp, false);
+		
+		/*
+		 * handle Customer( Customer cust, boolean fonance, Vechile vechile)
+		 * if( finance= true)
+		 * 		runcredit_history(Customer cust, double double_ amount)
+		 * 
+		 * else if(vechile.getprice()<= cust.cashonHand):
+		 * 		processTransaction(Customer cust,Vechile vechile)
+		 * 
+		 * else:
+		 * 		tell customer to bring more money
+		 * }
+		 * */
+				
+	}
+}
